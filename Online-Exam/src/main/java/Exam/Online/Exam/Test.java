1903a414352810e169b96ff7eb2f7ee930d613d8
K Arunkumar <92315975+Arun9677@users.noreply.github.com>
2022-03-20 15:03:21 +0530

    Add files via upload

---
+++ Online-Exam/src/main/java/Exam/Online/Exam/Test.java
@@ -0,0 +1,28 @@
+package Exam.Online.Exam;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test{
+	
+	@Id
+	long id;
+	String ans;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getAns() {
+		return ans;
+	}
+	public void setAns(String ans) {
+		this.ans = ans;
+	}
+
+}
